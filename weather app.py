fd8ea8b9fb7aeb95fc35443a36f2b339510997ec
hansika6812-creator <hansika6812@gmail.com>
2026-07-28 20:04:46 +0530

    Added python project

---
+++ weather app.py
@@ -0,0 +1,8 @@
+city = input("Enter city name: ")
+temperature = input("Enter temperature (°C): ")
+condition = input("Enter weather condition: ")
+
+print("\nWeather Report")
+print("City:", city)
+print("Temperature:", temperature, "°C")
+print("Condition:", condition)
